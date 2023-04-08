ca842c5137295761487abf2c111b90a9bca288d6
Kaio Rosa <kaiosrosa@gmail.com>
2023-04-07 21:01:13 -0300

    add useState

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ A Tic Tac Toe Game made following the React Tutorial on the official documentati
 ** 
 
 It's focused on the fundamentals with a simple but fun game, my intention was to cement the React basics with this one
+
+
+What was covered
+
+** Props
+** State 
+**
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,18 @@
-export default function Square({value}) {
+import { useState } from "react";
+
+export default function Square() {
+    const [value, setValue] = useState(null);
+    
+    function handleClick(){
+        setValue('X')
+    }
     return (
-        <button className="square">{value}</button>
+        <button 
+        className="square"
+        onClick={handleClick}
+        >
+            {value}
+        </button>
     );
     
   }
