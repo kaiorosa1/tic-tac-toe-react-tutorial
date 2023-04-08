0380825b3e1b67adc19c23740b71ede286c959a7
Kaio Rosa <kaiosrosa@gmail.com>
2023-04-07 21:25:00 -0300

    add alternating Xs and Os + calculateWinner

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,15 +1,9 @@
-import { useState } from "react";
-
-export default function Square() {
-    const [value, setValue] = useState(null);
-    
-    function handleClick(){
-        setValue('X')
-    }
+export default function Square({value, onSquareClick}) {
+   
     return (
         <button 
         className="square"
-        onClick={handleClick}
+        onClick={onSquareClick}
         >
             {value}
         </button>
